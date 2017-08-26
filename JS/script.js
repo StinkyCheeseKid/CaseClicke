5aa2f311bc446c3ab6732558aa5a05f0cd7ea29d
InFeRnO_w <couvreux_corentin@yahoo.fr>
2017-08-26 22:44:05 +0200

    Big update v0.1

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
-    $(".uspBtn").hide()
+    $(".uspBtn, .p250Btn").hide()
 
     function update () {
         $(".caseNumber").val(euroCount);
         document.title = euroCount +" € - CS:GO Farm";
         $(".glockBtn .clickerUpgInpt1").val(glockClicker);
         $(".uspBtn .clickerUpgInpt1").val(uspClicker);
+        $(".p250Btn .clickerUpgInpt1").val(p250Clicker);
     }
 
     var euroCount = 0;
@@ -15,19 +16,23 @@ $(document).ready(function() {
     var glockClickerPrice = 20;
     var uspClicker = 0;
     var uspClickerMultiplier = 0;
-    var uspClickerPrice = 45;
+    var uspClickerPrice = 30;
+    var p250Clicker = 0;
+    var p250ClickerMultiplier = 0;
+    var p250ClickerPrice = 45;
     var casePerSecJs = 0;
 
     function timer() {
         euroCount = euroCount + glockClicker;
         euroCount = euroCount + uspClickerMultiplier;
+        euroCount = euroCount + p250ClickerMultiplier;
         update();
     }
 
     setInterval(timer, 1000);
 
     $(".caseCsgoImg").mouseenter(function() {
-        $(this).css("transform", "scale(1.1, 1.1)");
+        $(this).css("transform", "scale(1.05, 1.05)");
     });
 
     $(".caseCsgoImg").mouseleave(function() {
@@ -39,7 +44,7 @@ $(document).ready(function() {
     });
 
     $(".caseCsgoImg").click(function() {
-        $(this).css("transform", "scale(0.9, 0.9)");
+        $(this).css("transform", "scale(0.95, 0.95)");
         euroCount = euroCount + euroCountAdd;
         $(".caseNumber").val(euroCount);
         euroCount = parseInt(euroCount);
@@ -115,7 +120,7 @@ $(".deleteBtn").click(function() {
         if (euroCount >= glockClickerPrice) {
             euroCount = euroCount - glockClickerPrice;
             glockClicker += 1;
-            glockClickerPrice *= 1.66;
+            glockClickerPrice *= 1.88;
             glockClickerPrice = parseInt(glockClickerPrice);
             euroCountAdd += 1;
             casePerSecJs += 1;
@@ -140,20 +145,44 @@ $(".deleteBtn").click(function() {
         if (euroCount >= uspClickerPrice) {
             euroCount = euroCount - uspClickerPrice;
             uspClicker += 1;
-            uspClickerPrice *= 1.66;
+            uspClickerPrice *= 1.88;
             uspClickerPrice = parseInt(uspClickerPrice);
             uspClickerMultiplier += 3;
-            euroCountAdd += 1;
+            euroCountAdd += 2;
             $(".casePerClick").val(euroCountAdd);
             casePerSecJs += 3;
             $(".uspBtn .clickerUpgInpt2").val(uspClickerPrice);
             $(".casePerSec").val(casePerSecJs);
+        if (upsClicker >= 15) {
+            $(".p250Btn").show();
+        }
             update();
         }
         else {
             $(".uspBtn .clickerUpgInpt2").css("color", "red")
             setTimeout(function() {
                 $(".uspBtn .clickerUpgInpt2").css("color", "black");
+            },500);
+        }
+    });
+    $(".p250Btn").click(function() {
+        if (euroCount >= uspClickerPrice) {
+            euroCount = euroCount - p250ClickerPrice;
+            p250Clicker += 1;
+            p250ClickerPrice *= 1.88;
+            p250ClickerPrice = parseInt(p250ClickerPrice);
+            p250ClickerMultiplier += 6;
+            euroCountAdd += 3;
+            $(".casePerClick").val(euroCountAdd);
+            casePerSecJs += 3;
+            $(".p250Btn .clickerUpgInpt2").val(p250ClickerPrice);
+            $(".casePerSec").val(casePerSecJs);
+            update();
+        }
+        else {
+            $(".p250Btn .clickerUpgInpt2").css("color", "red")
+            setTimeout(function() {
+                $(".p250Btn .clickerUpgInpt2").css("color", "black");
             },500);
         }
     });
