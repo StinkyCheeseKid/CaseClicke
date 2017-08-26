4a88ec977b8e63da22074e980a9314cc19fa280d
InFeRnO_w <couvreux_corentin@yahoo.fr>
2017-08-26 21:22:06 +0200

    Big update

--- JS/script.js
+++ JS/script.js
@@ -1,130 +1,130 @@
 $(document).ready(function() {
 
-    $(".farmUpg").hide()
+    $(".uspBtn").hide()
 
     function update () {
-        $(".cookieNumber").val(cookiecount);
-        document.title = cookiecount +" Cookies - Cookie Farm";
-        $(".autoClickBtn .clickerUpgInpt1").val(autoClicker);
-        $(".farmUpg .clickerUpgInpt1").val(farmClicker);
+        $(".caseNumber").val(euroCount);
+        document.title = euroCount +" € - CS:GO Farm";
+        $(".glockBtn .clickerUpgInpt1").val(glockClicker);
+        $(".uspBtn .clickerUpgInpt1").val(uspClicker);
     }
 
-    var cookiecount = 0;
-    var cookiecountAdd = 1;
-    var autoClicker = 0;
-    var autoClickerPrice = 20;
-    var farmClicker = 0;
-    var farmClickerMultiplier = 0;
-    var farmClickerPrice = 45;
-    var cookiePerSecJs = 0;
+    var euroCount = 0;
+    var euroCountAdd = 1;
+    var glockClicker = 0;
+    var glockClickerPrice = 20;
+    var uspClicker = 0;
+    var uspClickerMultiplier = 0;
+    var uspClickerPrice = 45;
+    var casePerSecJs = 0;
 
     function timer() {
-        cookiecount = cookiecount + autoClicker;
-        cookiecount = cookiecount + farmClickerMultiplier;
+        euroCount = euroCount + glockClicker;
+        euroCount = euroCount + uspClickerMultiplier;
         update();
     }
 
     setInterval(timer, 1000);
 
-    $(".perfectCookie").mouseenter(function() {
+    $(".caseCsgoImg").mouseenter(function() {
         $(this).css("transform", "scale(1.1, 1.1)");
     });
 
-    $(".perfectCookie").mouseleave(function() {
+    $(".caseCsgoImg").mouseleave(function() {
         $(this).css("transform", "none");
     });
 
-    $(".perfectCookie").hover(function() {
+    $(".caseCsgoImg").hover(function() {
         $(this).css("cursor", "pointer");
     });
 
-    $(".perfectCookie").click(function() {
+    $(".caseCsgoImg").click(function() {
         $(this).css("transform", "scale(0.9, 0.9)");
-        cookiecount = cookiecount + cookiecountAdd;
-        $(".cookieNumber").val(cookiecount);
-        cookiecount = parseInt(cookiecount);
-        document.title = cookiecount +" Cookies - Cookie Farm";
+        euroCount = euroCount + euroCountAdd;
+        $(".caseNumber").val(euroCount);
+        euroCount = parseInt(euroCount);
+        document.title = euroCount +" € - CS:GO Farm";
     });
     $(".saveBtn").click(function() {
-    localStorage.setItem("cookiecount", cookiecount);
-    localStorage.setItem("autoClicker", autoClicker);
-    localStorage.setItem("autoClickerPrice", autoClickerPrice);
-    localStorage.setItem("farmClicker", farmClicker);
-    localStorage.setItem("farmClickerPrice", farmClickerPrice);
-    localStorage.setItem("cookiePerSecJs", cookiePerSecJs);
-    localStorage.setItem("farmClickerMultiplier", farmClickerMultiplier);
-    localStorage.setItem("cookiecountAdd", cookiecountAdd);
+    localStorage.setItem("euroCount", euroCount);
+    localStorage.setItem("glockClicker", glockClicker);
+    localStorage.setItem("glockClickerPrice", glockClickerPrice);
+    localStorage.setItem("uspClicker", uspClicker);
+    localStorage.setItem("uspClickerPrice", uspClickerPrice);
+    localStorage.setItem("casePerSecJs", casePerSecJs);
+    localStorage.setItem("uspClickerMultiplier", uspClickerMultiplier);
+    localStorage.setItem("euroCountAdd", euroCountAdd);
 });
 
     $(".loadBtn").click(function() {
-    cookiecount = localStorage.getItem("cookiecount");
-    cookiecount = parseInt(cookiecount);
-    $(".cookieNumber").val(cookiecount);
-    autoClicker = localStorage.getItem("autoClicker");
-    autoClicker = parseInt(autoClicker);
-    $(".autoClickBtn .clickerUpgInpt1").val(autoClicker);
-    autoClickerPrice = localStorage.getItem("autoClickerPrice");
-    autoClickerPrice = parseInt(autoClickerPrice);
-    $(".autoClickBtn .clickerUpgInpt2").val(autoClickerPrice);
-    farmClicker = localStorage.getItem("farmClicker");
-    farmClicker = parseInt(farmClicker);
-    $(".farmUpg .clickerUpgInpt1").val(farmClicker);
-    farmClickerPrice = localStorage.getItem("farmClickerPrice");
-    farmClickerPrice = parseInt(farmClickerPrice);
-    $(".farmUpg .clickerUpgInpt2").val(farmClickerPrice);
-    farmClickerMultiplier = localStorage.getItem("farmClickerMultiplier");
-    farmClickerMultiplier = parseInt(farmClickerMultiplier);
-    cookiePerSecJs = localStorage.getItem("cookiePerSecJs");
-    cookiePerSecJs = parseInt(cookiePerSecJs);
-    $(".cookiePerSec").val(cookiePerSecJs);
-    cookiecountAdd = localStorage.getItem("cookiecountAdd");
-    cookiecountAdd = parseInt(cookiecountAdd);
-    $("#cookieSpot .cookiePerClickSpot .cookiePerClick").val(cookiecountAdd);
-    document.title = cookiecount +" Cookies - Cookie Farm";
+    euroCount = localStorage.getItem("euroCount");
+    euroCount = parseInt(euroCount);
+    $(".caseNumber").val(euroCount);
+    glockClicker = localStorage.getItem("glockClicker");
+    glockClicker = parseInt(glockClicker);
+    $(".glockBtn .clickerUpgInpt1").val(glockClicker);
+    glockClickerPrice = localStorage.getItem("glockClickerPrice");
+    glockClickerPrice = parseInt(glockClickerPrice);
+    $(".glockBtn .clickerUpgInpt2").val(glockClickerPrice);
+    uspClicker = localStorage.getItem("uspClicker");
+    uspClicker = parseInt(uspClicker);
+    $(".uspBtn .clickerUpgInpt1").val(uspClicker);
+    uspClickerPrice = localStorage.getItem("uspClickerPrice");
+    uspClickerPrice = parseInt(uspClickerPrice);
+    $(".uspBtn .clickerUpgInpt2").val(uspClickerPrice);
+    uspClickerMultiplier = localStorage.getItem("uspClickerMultiplier");
+    uspClickerMultiplier = parseInt(uspClickerMultiplier);
+    casePerSecJs = localStorage.getItem("casePerSecJs");
+    casePerSecJs = parseInt(casePerSecJs);
+    $(".casePerSec").val(casePerSecJs);
+    euroCountAdd = localStorage.getItem("euroCountAdd");
+    euroCountAdd = parseInt(euroCountAdd);
+    $("#cookieSpot .casePerClickSpot .casePerClick").val(euroCountAdd);
+    document.title = euroCount +" € - CS:GO Farm";
     update();
 });
 
 $(".resetBtn").click(function() {
-    cookiecount = 0;
-    autoClicker = 0;
-    autoClickerPrice = 20;
-    $(".autoClickBtn .clickerUpgInpt2").val(autoClickerPrice);
-    farmClicker = 0;
-    farmClickerMultiplier = 0;
-    farmClickerPrice = 50;
-    $(".farmUpg .clickerUpgInpt2").val(farmClickerPrice);
-    cookiePerSecJs = 0;
-    $(".cookiePerSec").val(cookiePerSecJs);
-    cookiecountAdd = 1;
-    $(".cookiePerClick").val(cookiecountAdd);
+    euroCount = 0;
+    glockClicker = 0;
+    glockClickerPrice = 20;
+    $(".glockBtn .clickerUpgInpt2").val(glockClickerPrice);
+    uspClicker = 0;
+    uspClickerMultiplier = 0;
+    uspClickerPrice = 50;
+    $(".uspBtn .clickerUpgInpt2").val(uspClickerPrice);
+    casePerSecJs = 0;
+    $(".casePerSec").val(casePerSecJs);
+    euroCountAdd = 1;
+    $(".casePerClick").val(euroCountAdd);
 });
 
 $(".deleteBtn").click(function() {
-    localStorage.removeItem("cookiecount", cookiecount);
-    localStorage.removeItem("autoClicker", autoClicker);
-    localStorage.removeItem("autoClickerPrice", autoClickerPrice);
-    localStorage.removeItem("farmClicker", farmClicker);
-    localStorage.removeItem("farmClickerPrice", farmClickerPrice);
-    localStorage.removeItem("cookiePerSecJs", cookiePerSecJs);
-    localStorage.removeItem("farmClickerMultiplier", farmClickerMultiplier);
-    localStorage.removeItem("cookiecountAdd", cookiecountAdd);
+    localStorage.removeItem("euroCount", euroCount);
+    localStorage.removeItem("glockClicker", glockClicker);
+    localStorage.removeItem("glockClickerPrice", glockClickerPrice);
+    localStorage.removeItem("uspClicker", uspClicker);
+    localStorage.removeItem("uspClickerPrice", uspClickerPrice);
+    localStorage.removeItem("casePerSecJs", casePerSecJs);
+    localStorage.removeItem("uspClickerMultiplier", uspClickerMultiplier);
+    localStorage.removeItem("euroCountAdd", euroCountAdd);
 });
 
 
-    $(".autoClickBtn").click(function() {
-        if (cookiecount >= autoClickerPrice) {
-            cookiecount = cookiecount - autoClickerPrice;
-            autoClicker += 1;
-            autoClickerPrice *= 1.66;
-            autoClickerPrice = parseInt(autoClickerPrice);
-            cookiecountAdd += 1;
-            cookiePerSecJs += 1;
-            $(".autoClickBtn .clickerUpgInpt2").val(autoClickerPrice);
-            $(".cookiePerClick").val(cookiecountAdd);
-            cookiecountAdd = parseInt(cookiecountAdd);
-            $(".cookiePerSec").val(cookiePerSecJs);
-        if (autoClicker >= 10) {
-            $(".farmUpg").show();
+    $(".glockBtn").click(function() {
+        if (euroCount >= glockClickerPrice) {
+            euroCount = euroCount - glockClickerPrice;
+            glockClicker += 1;
+            glockClickerPrice *= 1.66;
+            glockClickerPrice = parseInt(glockClickerPrice);
+            euroCountAdd += 1;
+            casePerSecJs += 1;
+            $(".glockBtn .clickerUpgInpt2").val(glockClickerPrice);
+            $(".casePerClick").val(euroCountAdd);
+            euroCountAdd = parseInt(euroCountAdd);
+            $(".casePerSec").val(casePerSecJs);
+        if (glockClicker >= 10) {
+            $(".uspBtn").show();
         }
             update();
         }
@@ -136,24 +136,24 @@ $(".deleteBtn").click(function() {
         }
     });
 
-    $(".farmUpg").click(function() {
-        if (cookiecount >= farmClickerPrice) {
-            cookiecount = cookiecount - farmClickerPrice;
-            farmClicker += 1;
-            farmClickerPrice *= 1.66;
-            farmClickerPrice = parseInt(farmClickerPrice);
-            farmClickerMultiplier += 3;
-            cookiecountAdd += 1;
-            $(".cookiePerClick").val(cookiecountAdd);
-            cookiePerSecJs += 3;
-            $(".farmUpg .clickerUpgInpt2").val(farmClickerPrice);
-            $(".cookiePerSec").val(cookiePerSecJs);
+    $(".uspBtn").click(function() {
+        if (euroCount >= uspClickerPrice) {
+            euroCount = euroCount - uspClickerPrice;
+            uspClicker += 1;
+            uspClickerPrice *= 1.66;
+            uspClickerPrice = parseInt(uspClickerPrice);
+            uspClickerMultiplier += 3;
+            euroCountAdd += 1;
+            $(".casePerClick").val(euroCountAdd);
+            casePerSecJs += 3;
+            $(".uspBtn .clickerUpgInpt2").val(uspClickerPrice);
+            $(".casePerSec").val(casePerSecJs);
             update();
         }
         else {
-            $(".farmUpg .clickerUpgInpt2").css("color", "red")
+            $(".uspBtn .clickerUpgInpt2").css("color", "red")
             setTimeout(function() {
-                $(".farmUpg .clickerUpgInpt2").css("color", "black");
+                $(".uspBtn .clickerUpgInpt2").css("color", "black");
             },500);
         }
     });
