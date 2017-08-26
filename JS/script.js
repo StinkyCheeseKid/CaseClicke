02de3d85db95da3d1eea9bfece426926be92b0d8
InFeRnO_w <couvreux_corentin@yahoo.fr>
2017-08-26 18:47:21 +0200

    Amélioration du site (Ajouts de 2 buttons, améliorations déblocable, ajouts de nouveaux compteurs

--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,51 @@
 $(document).ready(function() {
+
+        $(".farmUpg").hide()
+
     function update () {
         $(".cookieNumber").val(cookiecount);
         document.title = cookiecount +" Cookies - Cookie Farm";
         $(".autoClickBtn .clickerUpgInpt1").val(autoClicker);
         $(".farmUpg .clickerUpgInpt1").val(farmClicker);
     }
+
     var cookiecount = 0;
+    var cookiecountAdd = 1;
     var autoClicker = 0;
     var autoClickerPrice = 20;
     var farmClicker = 0;
     var farmClickerMultiplier = 0;
     var farmClickerPrice = 45;
     var cookiePerSecJs = 0;
+
     function timer() {
         cookiecount = cookiecount + autoClicker;
         cookiecount = cookiecount + farmClickerMultiplier;
         update();
     }
-    setInterval(timer, 1000)
+
+    setInterval(timer, 1000);
+
     $(".perfectCookie").mouseenter(function() {
         $(this).css("transform", "scale(1.1, 1.1)");
     });
+
     $(".perfectCookie").mouseleave(function() {
         $(this).css("transform", "none");
     });
+
     $(".perfectCookie").hover(function() {
         $(this).css("cursor", "pointer");
     });
+
     $(".perfectCookie").click(function() {
         $(this).css("transform", "scale(0.9, 0.9)");
-        cookiecount = cookiecount + 1;
+        cookiecount = cookiecount + cookiecountAdd;
         $(".cookieNumber").val(cookiecount);
         cookiecount = parseInt(cookiecount);
         document.title = cookiecount +" Cookies - Cookie Farm";
     });
+
     $(".saveBtn").click(function() {
         localStorage.setItem("cookiecount", cookiecount);
         localStorage.setItem("autoClicker", autoClicker);
@@ -41,7 +53,10 @@ $(document).ready(function() {
         localStorage.setItem("farmClicker", farmClicker);
         localStorage.setItem("farmClickerPrice", farmClickerPrice);
         localStorage.setItem("cookiePerSecJs", cookiePerSecJs);
+        localStorage.setItem("farmClickerMultiplier", farmClickerMultiplier);
+        localStorage.setItem("cookiecountAdd", cookiecountAdd);
     });
+
     $(".loadBtn").click(function() {
         cookiecount = localStorage.getItem("cookiecount");
         cookiecount = parseInt(cookiecount);
@@ -58,20 +73,53 @@ $(document).ready(function() {
         farmClickerPrice = localStorage.getItem("farmClickerPrice");
         farmClickerPrice = parseInt(farmClickerPrice);
         $(".farmUpg .clickerUpgInpt2").val(farmClickerPrice);
+        farmClickerMultiplier = localStorage.getItem("farmClickerMultiplier");
+        farmClickerMultiplier = parseInt(farmClickerMultiplier);
         cookiePerSecJs = localStorage.getItem("cookiePerSecJs");
         cookiePerSecJs = parseInt(cookiePerSecJs);
         $(".cookiePerSec").val(cookiePerSecJs);
+        cookiecountAdd = localStorage.getItem("cookiecountAdd");
+        cookiecountAdd = parseInt(cookiecountAdd);
+        $("#cookieSpot .cookiePerClickSpot .cookiePerClick").val(cookiecountAdd);
         document.title = cookiecount +" Cookies - Cookie Farm";
         update();
     });
+
+    $(".resetBtn").click(function() {
+        cookiecount = 0;
+        autoClicker = 0;
+        autoClickerPrice = 20;
+        $(".autoClickBtn .clickerUpgInpt2").val(autoClickerPrice);
+        farmClicker = 0;
+        farmClickerMultiplier = 0;
+        farmClickerPrice = 50;
+        $(".farmUpg .clickerUpgInpt2").val(farmClickerPrice);
+        cookiePerSecJs = 0;
+        $(".cookiePerSec").val(cookiePerSecJs);
+    });
+
+    $(".deleteBtn").click(function() {
+        localStorage.removeItem("cookiecount", cookiecount);
+        localStorage.removeItem("autoClicker", autoClicker);
+        localStorage.removeItem("autoClickerPrice", autoClickerPrice);
+        localStorage.removeItem("farmClicker", farmClicker);
+        localStorage.removeItem("farmClickerPrice", farmClickerPrice);
+        localStorage.removeItem("cookiePerSecJs", cookiePerSecJs);
+        localStorage.removeItem("farmClickerMultiplier", farmClickerMultiplier);
+        localStorage.removeItem("cookiecountAdd", cookiecountAdd);
+    });
+
     $(".autoClickBtn").click(function() {
         if (cookiecount >= autoClickerPrice) {
             cookiecount = cookiecount - autoClickerPrice;
             autoClicker += 1;
             autoClickerPrice *= 1.66;
             autoClickerPrice = parseInt(autoClickerPrice);
+            cookiecountAdd += 1;
             cookiePerSecJs += 1;
             $(".autoClickBtn .clickerUpgInpt2").val(autoClickerPrice);
+            $(".cookiePerClick").val(cookiecountAdd);
+            cookiecountAdd = parseInt(cookiecountAdd);
             $(".cookiePerSec").val(cookiePerSecJs);
             update();
         }
@@ -79,9 +127,10 @@ $(document).ready(function() {
             $(".clickerUpgInpt2").css("color", "red")
             setTimeout(function() {
                 $(".clickerUpgInpt2").css("color", "black");
-            },500);
+            },400);
         }
     });
+    
     $(".farmUpg").click(function() {
         if (cookiecount >= farmClickerPrice) {
             cookiecount = cookiecount - farmClickerPrice;
@@ -89,6 +138,8 @@ $(document).ready(function() {
             farmClickerPrice *= 1.66;
             farmClickerPrice = parseInt(farmClickerPrice);
             farmClickerMultiplier += 3;
+            cookiecountAdd += 1;
+            $(".cookiePerClick").val(cookiecountAdd);
             cookiePerSecJs += 3;
             $(".farmUpg .clickerUpgInpt2").val(farmClickerPrice);
             $(".cookiePerSec").val(cookiePerSecJs);
