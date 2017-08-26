1482b09d8a8fa4b580c408f21ca86faaf618cce5
InFeRnO_w <couvreux_corentin@yahoo.fr>
2017-08-26 14:52:17 +0200

    Finalisation des 2 Améliorations (Clicker et Farm) en les synchronisants à tous les autres compteurs

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,104 @@
 $(document).ready(function() {
-    var cookiecount = 0;
-    $(".perfectCookie").click(function() {
-        cookiecount = cookiecount + 1;
+    function update () {
         $(".cookieNumber").val(cookiecount);
         document.title = cookiecount +" Cookies - Cookie Farm";
-        console.log(cookiecount);
+        $(".autoClickBtn .clickerUpgInpt1").val(autoClicker);
+        $(".farmUpg .clickerUpgInpt1").val(farmClicker);
+    }
+    var cookiecount = 0;
+    var autoClicker = 0;
+    var autoClickerPrice = 20;
+    var farmClicker = 0;
+    var farmClickerMultiplier = 0;
+    var farmClickerPrice = 45;
+    var cookiePerSecJs = 0;
+    function timer() {
+        cookiecount = cookiecount + autoClicker;
+        cookiecount = cookiecount + farmClickerMultiplier;
+        update();
+    }
+    setInterval(timer, 1000)
+    $(".perfectCookie").mouseenter(function() {
+        $(this).css("transform", "scale(1.1, 1.1)");
+    });
+    $(".perfectCookie").mouseleave(function() {
+        $(this).css("transform", "none");
+    });
+    $(".perfectCookie").hover(function() {
+        $(this).css("cursor", "pointer");
+    });
+    $(".perfectCookie").click(function() {
+        $(this).css("transform", "scale(0.9, 0.9)");
+        cookiecount = cookiecount + 1;
+        $(".cookieNumber").val(cookiecount);
+        cookiecount = parseInt(cookiecount);
+        document.title = cookiecount +" Cookies - Cookie Farm";
+    });
+    $(".saveBtn").click(function() {
+        localStorage.setItem("cookiecount", cookiecount);
+        localStorage.setItem("autoClicker", autoClicker);
+        localStorage.setItem("autoClickerPrice", autoClickerPrice);
+        localStorage.setItem("farmClicker", farmClicker);
+        localStorage.setItem("farmClickerPrice", farmClickerPrice);
+        localStorage.setItem("cookiePerSecJs", cookiePerSecJs);
+    });
+    $(".loadBtn").click(function() {
+        cookiecount = localStorage.getItem("cookiecount");
+        cookiecount = parseInt(cookiecount);
+        $(".cookieNumber").val(cookiecount);
+        autoClicker = localStorage.getItem("autoClicker");
+        autoClicker = parseInt(autoClicker);
+        $(".autoClickBtn .clickerUpgInpt1").val(autoClicker);
+        autoClickerPrice = localStorage.getItem("autoClickerPrice");
+        autoClickerPrice = parseInt(autoClickerPrice);
+        $(".autoClickBtn .clickerUpgInpt2").val(autoClickerPrice);
+        farmClicker = localStorage.getItem("farmClicker");
+        farmClicker = parseInt(farmClicker);
+        $(".farmUpg .clickerUpgInpt1").val(farmClicker);
+        farmClickerPrice = localStorage.getItem("farmClickerPrice");
+        farmClickerPrice = parseInt(farmClickerPrice);
+        $(".farmUpg .clickerUpgInpt2").val(farmClickerPrice);
+        cookiePerSecJs = localStorage.getItem("cookiePerSecJs");
+        cookiePerSecJs = parseInt(cookiePerSecJs);
+        $(".cookiePerSec").val(cookiePerSecJs);
+        document.title = cookiecount +" Cookies - Cookie Farm";
+        update();
+    });
+    $(".autoClickBtn").click(function() {
+        if (cookiecount >= autoClickerPrice) {
+            cookiecount = cookiecount - autoClickerPrice;
+            autoClicker += 1;
+            autoClickerPrice *= 1.66;
+            autoClickerPrice = parseInt(autoClickerPrice);
+            cookiePerSecJs += 1;
+            $(".autoClickBtn .clickerUpgInpt2").val(autoClickerPrice);
+            $(".cookiePerSec").val(cookiePerSecJs);
+            update();
+        }
+        else {
+            $(".clickerUpgInpt2").css("color", "red")
+            setTimeout(function() {
+                $(".clickerUpgInpt2").css("color", "black");
+            },500);
+        }
+    });
+    $(".farmUpg").click(function() {
+        if (cookiecount >= farmClickerPrice) {
+            cookiecount = cookiecount - farmClickerPrice;
+            farmClicker += 1;
+            farmClickerPrice *= 1.66;
+            farmClickerPrice = parseInt(farmClickerPrice);
+            farmClickerMultiplier += 3;
+            cookiePerSecJs += 3;
+            $(".farmUpg .clickerUpgInpt2").val(farmClickerPrice);
+            $(".cookiePerSec").val(cookiePerSecJs);
+            update();
+        }
+        else {
+            $(".farmUpg .clickerUpgInpt2").css("color", "red")
+            setTimeout(function() {
+                $(".farmUpg .clickerUpgInpt2").css("color", "black");
+            },500);
+        }
     });
 });
