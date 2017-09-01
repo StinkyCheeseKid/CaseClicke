621a90944eedc051722519edbd9970da7411c9a2
InFeRnO_w <couvreux_corentin@yahoo.fr>
2017-09-02 00:52:10 +0200

    Minor fix

---
+++ register.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+            <title>CS:GO Farm</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+            <link rel="stylesheet" href="CSS/styleRegister.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="JS/scriptRegister.js"></script>
+    </head>
+<body>
+    <div id="loginBox">
+        <div class="iCenter">
+                <input class="username" type="text" maxlength="50" placeholder="Username">
+                <input type="text" maxlength="50" placeholder="Password"><br>
+                <input type="text" maxlength="50" placeholder="Email"><br>
+            <p style="color:#fff"><br/><strong><em>Are you over 18 years old ?</em></strong></p>
+                <p style="color: white;">Yes</p>
+                <input class="checkEighteenYes" type="checkbox" maxlength="50">
+                <p style="color: white;">No</p>
+                <input class="checkEighteenNo" type="checkbox" maxlength="50"><br>
+            <button class="submitBtn btn btn-info" type="submit">Register</button>
+        </div>
+    </div>
+    <footer class="text-center">
+            <nav class="navbar navbar-default navbar-fixed-bottom">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="clicker.html">Clicker</a></li>
+                            <li><a href="#">...</a></li>
+                            <li><a href="#">...</a></li>
+                            <li><a href="#">...</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </nav>
+
+        </footer>
+</body>
+</html>
